8ae42ac78cdb66dcefd71ccb4e16544948b67900
woobeen.lim <woobeen@adsl-172-10-101-136.dsl.hstntx.sbcglobal.net>
2021-03-12 11:54:59 +0900

    Project Init

--- src/main/java/BaseBallApplication.java
+++ src/main/java/BaseBallApplication.java
@@ -4,8 +4,5 @@ import java.util.List;
 public class BaseBallApplication {
     public static void main(String[] args) {
         System.out.println("BaseBallApplication");
-
-        List<Integer> list = new ArrayList<>(3);
-
     }
 }
--- src/main/java/game/rule/Result.java
+++
@@ -1,7 +0,0 @@
-package game.rule;
-
-import java.util.List;
-
-public interface Result {
-    void execute(List<Integer> myNumbers , List<Integer> otherNumbers);
-}
