fb26a70c24aa1f229cc62ae8f069aee8e11d8846
woobeen.lim <drogba02@naver.com>
2021-03-24 16:29:26 +0900

    Add - ExceptionType 추가

---
+++ src/main/java/domain/ExceptionType.java
@@ -0,0 +1,16 @@
+package domain;
+
+public enum ExceptionType {
+    INVALID_INPUT_NUMBER_SIZE("3개의 숫자만 입력할 수 있습니다."),
+    REQUIRED_NUMBER_TYPES("숫자만 입력하여야 합니다.");
+
+    private final String message;
+
+    ExceptionType(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
