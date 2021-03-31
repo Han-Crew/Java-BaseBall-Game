c48d5f7e3f8e20194901e64c9246fa09137e3863
woobeen.lim <drogba02@naver.com>
2021-03-31 16:24:01 +0900

    Add - 숫자야구 클론코딩  (자바지기 버전)

--- src/main/java/javajigi/PlayResult.java
+++ src/main/java/javajigi/PlayResult.java
@@ -1,2 +1,26 @@
-package javajigi;public class PlayResult {
+package javajigi;
+
+public class PlayResult {
+    private int strike = 0;
+    private int ball = 0;
+
+    public int getStrike() {
+        return this.strike;
+    }
+
+    public int getBall() {
+        return this.ball;
+    }
+
+    public void report(BallStatus status) {
+        if (status.isStrike()) {
+            this.strike += 1;
+        } else if (status.isBall()) {
+            this.ball += 1;
+        }
+    }
+
+    public boolean isGameEnd() {
+        return this.strike == 3;
+    }
 }
--- src/test/java/javajigi/BallsTest.java
+++ src/test/java/javajigi/BallsTest.java
@@ -1,2 +1,55 @@
-package javajigi;public class BallsTest {
+package javajigi;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BallsTest {
+    @Test
+    void play_out() {
+        Balls answers = new Balls(List.of(1 , 2 , 3));
+        PlayResult result = answers.play(List.of(4 , 5 , 6));
+        assertThat(result.getStrike()).isEqualTo(0);
+        assertThat(result.getBall()).isEqualTo(0);
+    }
+
+    @Test
+    void play_1strike_1ball() {
+        Balls answers = new Balls(List.of(1 , 2 , 3));
+        PlayResult result = answers.play(List.of(1 , 4 , 2));
+        assertThat(result.getStrike()).isEqualTo(1);
+        assertThat(result.getBall()).isEqualTo(1);
+    }
+
+    @Test
+    void play_3strike() {
+        Balls answers = new Balls(List.of(1 , 2 , 3));
+        PlayResult result = answers.play(List.of(1 , 2 , 3));
+        assertThat(result.getStrike()).isEqualTo(3);
+        assertThat(result.getBall()).isEqualTo(0);
+        assertThat(result.isGameEnd()).isTrue();
+    }
+
+    @Test
+    void STRIKE() {
+        Balls answers = new Balls(List.of(1 , 2 , 3));
+        BallStatus status = answers.play(new Ball(1 , 1));
+        assertThat(status).isEqualTo(BallStatus.STRIKE);
+    }
+
+    @Test
+    void BALL() {
+        Balls answers = new Balls(List.of(1 , 2 , 3));
+        BallStatus status = answers.play(new Ball(1 , 2));
+        assertThat(status).isEqualTo(BallStatus.BALL);
+    }
+
+    @Test
+    void OUT() {
+        Balls answers = new Balls(List.of(1 , 2 , 3));
+        BallStatus status = answers.play(new Ball(1 , 4));
+        assertThat(status).isEqualTo(BallStatus.OUT);
+    }
 }
